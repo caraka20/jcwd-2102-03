05760f45a8b61bca0de160ccb94c98b599ed67fe
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2022-08-25 17:46:15 +0700

    Update deploy-fe.config.js

--- deploy-fe.config.js
+++ deploy-fe.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   apps: [
     {
-      name: 'JCWD-2000-01-FE', // Format JCWD-{batchcode}-{groupnumber}
+      name: 'JCWD-2102-03-FE', // Format JCWD-{batchcode}-{groupnumber}
       script: 'npm',
       args: 'start',
       env: {
         NODE_ENV: 'production',
-        PORT: 3000, //format groupnumber and batch ex: 3401
+        PORT: 3203, //format groupnumber and batch ex: 3401
       },
     },
   ],
