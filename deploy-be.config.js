ae43036e46b707f95e3833ca65e114fed97ff67c
Purwadhika Fullstack <93514408+purwadhikafullstack@users.noreply.github.com>
2022-08-25 17:45:53 +0700

    Update deploy-be.config.js

--- deploy-be.config.js
+++ deploy-be.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'JCWD-2000-01-BE', // Format JCWD-{batchcode}-{groupnumber}
+      name: 'JCWD-2102-03-BE', // Format JCWD-{batchcode}-{groupnumber}
       script: './packages/server/src/index.js',
       env: {
         NODE_ENV: 'production',
-        PORT: 8000, //format groupnumber and batch ex: 8401
+        PORT: 8203, //format groupnumber and batch ex: 8401
       },
       time: true,
     },
